1b3181c95f2bc154e706bc703c2a850792dbb2b4
Mike Austin <mike_ekim@yahoo.com>
2021-06-26 22:13:36 -0700

    Add menu css and JS interaction

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,25 +1,41 @@
 import React, { useRef, useState, useEffect } from 'react';
 
 import { View, Text, Image, Button, Spacer, Divider, List, Heading, Clickable, Window } from '.';
+import styles from './MenuBar.module.css';
 
 const MenuBar = () => {
+  const menuRef = useRef(null);
+
+  const handleClick = (event) => {
+    console.log(event.target.offsetParent.offsetParent.getBoundingClientRect().x);
+
+    menuRef.current.style.left = event.target.offsetParent.offsetParent.getBoundingClientRect().x + 'px';
+  };
+
   return (
     <>
       <List horizontal xpadding="medium" horizontalPadding="small" spacerSize="none" background="gray-1">
-        <View padding="small" horizontalPadding="medium" background="blue-5">
-          <Text fontWeight="bold" color="white">React WM</Text>
+        <View padding="small" horizontalPadding="medium" className={styles.menuItem} tabIndex="0" onClick={handleClick}>
+          <Text fontWeight="bold">React WM</Text>
         </View>
-        <View padding="small" horizontalPadding="medium">
+        <View padding="small" horizontalPadding="medium" className={styles.menuItem} tabIndex="0" onClick={handleClick}>
           <Text fontWeight="medium">React WM</Text>
         </View>
-        <View padding="small" horizontalPadding="medium">
+        <View padding="small" horizontalPadding="medium" className={styles.menuItem} tabIndex="0" onClick={handleClick}>
           <Text fontWeight="medium">React WM</Text>
         </View>
       </List>
-      <View verticalPadding="xsmall" background="gray-1" boxShadow style={{ position: 'absolute', zIndex: 2, left: 10, top: 30 }}>
-        <View padding="small" horizontalPadding="medium" background="blue-5"><Text fontWeight="semibold" color="white">Menu Option One</Text></View>
-        <View padding="small" horizontalPadding="small"><Text fontWeight="semibold">Menu Option One</Text></View>
-        <View padding="small" horizontalPadding="small"><Text fontWeight="semibold">Menu Option One</Text></View>
+
+      <View ref={menuRef} verticalPadding="xsmall" background="gray-1" boxShadow style={{ position: 'absolute', zIndex: 2, left: 10, top: 30 }}>
+        <View padding="small" horizontalPadding="medium" className={styles.menuItem} tabIndex="0">
+          <Text fontWeight="semibold">Menu Option One</Text>
+        </View>
+        <View padding="small" horizontalPadding="medium" className={styles.menuItem}>
+          <Text fontWeight="semibold">Menu Option One</Text>
+        </View>
+        <View padding="small" horizontalPadding="medium" className={styles.menuItem}>
+          <Text fontWeight="semibold">Menu Option One</Text>
+        </View>
       </View>
     </>
   );
