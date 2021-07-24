d38a3b3fdb7e6060a97bb9513807c66f40dd47ba
Mike Austin <mike_ekim@yahoo.com>
2021-07-23 20:44:40 -0700

    Half the frame rate

--- src/widgets/Fountain.js
+++ src/widgets/Fountain.js
@@ -10,8 +10,8 @@ const createParticles = () => Array.from({ length: 1000 }, (_, index) => (
       y: 200,
     },
     vel: {
-      x: Math.random() * -2.0 + 1,
-      y: Math.random() * -5.0 + 2,
+      x: Math.random() * -2.0 + 1.0,
+      y: Math.random() * -4.0 + 3,
     }
   })
 );
@@ -19,6 +19,7 @@ const createParticles = () => Array.from({ length: 1000 }, (_, index) => (
 const Fountain = () => {
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
+  const frameRef = useRef(0);
   const particlesRef = useRef(useMemo(() => createParticles()));
   const lastTimestamp = useRef(null);
 
@@ -31,39 +32,38 @@ const Fountain = () => {
       lastTimestamp = timestamp;
     }
 
-    const delta = (timestamp - lastTimestamp.current) / 16;
+    if (frameRef.current % 2 === 0) {
+      const delta = (timestamp - lastTimestamp.current) / 16;
 
-    // console.log(delta);
+      contextRef.current.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
 
-    contextRef.current.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+      for (let particle of particlesRef.current) {
+        contextRef.current.fillStyle = `hsla(30, 100%, 50%, ${particle.ttl / 1000})`;
 
-    for (let particle of particlesRef.current) {
-      contextRef.current.fillStyle = `hsla(30, 100%, 50%, ${particle.ttl / 1000})`;
+        contextRef.current.beginPath();
+        contextRef.current.arc(particle.pos.x, particle.pos.y, 1, 0, 2 * Math.PI);
+        contextRef.current.fill();
 
-      // console.log(particle.ttl);
+        particle.ttl -= 5 * delta;
+        particle.pos.x += particle.vel.x * delta;
+        particle.pos.y += particle.vel.y * delta;
+        particle.vel.y += 0.1 * delta;
 
-      contextRef.current.beginPath();
-      contextRef.current.arc(particle.pos.x, particle.pos.y, 1, 0, 2 * Math.PI);
-      contextRef.current.fill();
-
-      particle.ttl -= 5 * delta;
-      particle.pos.x += particle.vel.x * delta;
-      particle.pos.y += particle.vel.y * delta;
-      particle.vel.y += 0.1 * delta;
-
-      if (particle.pos.y > 250) {
-        if (particle.vel.y < 1) {
-          particle.ttl = Math.random() * 300 + 700;
-          particle.pos.x = 100;
-          particle.pos.y = 250;
-          particle.vel.x = Math.random() * -2 + 1;
-          particle.vel.y = Math.random() * -4.0 - 3;
-        } else {
-          particle.vel.y *= -0.2 * delta;
+        if (particle.pos.y > 250) {
+          if (particle.vel.y < 1) {
+            particle.ttl = Math.random() * 300 + 700;
+            particle.pos.x = 100;
+            particle.pos.y = 250;
+            particle.vel.x = Math.random() * -2 + 1.0;
+            particle.vel.y = Math.random() * -4.0 - 3.0;
+          } else {
+            particle.vel.y *= -0.2 * delta;
+          }
         }
       }
     }
 
+    frameRef.current++;
     lastTimestamp.current = timestamp;
 
     window.requestAnimationFrame(handleAnimationFrame);
